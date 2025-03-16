a060a3a47d00b8f7678bb640f3beba1f0319c481
Onjospiat <guilherme.chini.gani@gmail.com>
2025-03-16 15:03:55 -0300

    Final da aula 18 usando JS para fazer overlay

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function rolagem(id_secao){
 }
 
 window.rolagem = rolagem;
+
+function overlay(img_element){
+    console.log(img_element.offsetWidth);
+    console.log(img_element.offsetHeight);
+    
+}
+
+window.overlay = overlay;
+
+function func_overlay(elemento,i){
+    const text_over = document.getElementsByClassName("overlay-text");
+    text_over[i].style.width = elemento.offsetWidth + "px";
+    text_over[i].style.height = elemento.offsetHeight + "px";
+}
+
+window.func_overlay = func_overlay;
